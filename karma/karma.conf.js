0abb9022fbdfc24cd11e317291a3aae4d9b03c86
Alex Browne <stephenalexbrowne@gmail.com>
2015-02-27 19:36:52 -0500

    Configure Karma to use Chrome, Safari, and Firefox, but not to include PhantomJS.
    PhantomJS is incompatible with gopherjs due to the lack of support for certain
    typed arrays. See https://github.com/ariya/phantomjs/issues/10607

--- karma/karma.conf.js
+++ karma/karma.conf.js
@@ -56,8 +56,9 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: [
-            'PhantomJS',
-            'Chrome'
+            'Chrome',
+            'Firefox',
+            'Safari'
         ],
 
 
