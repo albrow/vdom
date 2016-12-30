d903b3558a38fe7836e91d731c4b373641c2674b
Damien Radtke <damien@buildthis.net>
2016-12-29 22:49:41 -0600

    Fix for GopherJS HierarchyRequestError

--- patch.go
+++ patch.go
@@ -103,6 +103,22 @@ func (p *Remove) Patch(root dom.Element) error {
 	}
 	self := findInDOM(p.Node, root)
 	parent.RemoveChild(self)
+
+	// p.Node was removed, so subtract one from the final index for all
+	// siblings that come after it.
+	lastIndex := p.Node.Index()[len(p.Node.Index())-1]
+	for _, sibling := range p.Node.Parent().Children()[lastIndex:] {
+		switch t := sibling.(type) {
+		case *Element:
+			t.index[len(t.index)-1] = t.index[len(t.index)-1] - 1
+		case *Text:
+			t.index[len(t.index)-1] = t.index[len(t.index)-1] - 1
+		case *Comment:
+			t.index[len(t.index)-1] = t.index[len(t.index)-1] - 1
+		default:
+			panic("unreachable")
+		}
+	}
 	return nil
 }
 
